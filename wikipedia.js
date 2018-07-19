90737b92726f7d026f0175fa2f8f51acb2fa9d34
P-Luca <P-Luca@users.noreply.github.com>
2018-07-19 11:40:45 +0200

    Update wikipedia.js

--- wikipedia.js
+++ wikipedia.js
@@ -3,9 +3,15 @@ function Wikipedia (lang) {
 }
 
 Wikipedia.prototype.search = function(query) {
-  var result = http().get("https://"+this.lang+".wikipedia.org/w/api.php?action=query&format=json&prop=coordinates%7Cdescription&titles=" + encodeURIComponent(query));
+  var result = http().get("https://"+this.lang+".wikipedia.org/w/api.php?action=query&format=json&prop=coordinates%7Cdescription%7Cimages%7Cimageinfo&callback=?&titles=" + encodeURIComponent(query));
   var json = JSON.parse(result);
-  return json;  
+  var pages = json.query.pages;
+  var resultArray = [];
+  for (var id in pages) {
+    var page = pages[id];
+    resultArray.push(page);
+  }
+  return resultArray;
 }
 
 /**
