3574e0f4f49b33fe627187750cf180bf7b41afe4
P-Luca <P-Luca@users.noreply.github.com>
2018-07-20 09:50:06 +0200

    Update wikipedia.js

--- wikipedia.js
+++ wikipedia.js
@@ -23,3 +23,26 @@ Wikipedia.prototype.search = function(query) {
   }
   return resultArray;
 }
+
+Wikipedia.prototype.details = function(pageId) {
+  //cleanText = strInputCode.replace(/<\/?[^>]+(>|$)/g, "");
+  var result = http().get("https://"+this.lang+".wikipedia.org/w/api.php?action=query&format=json&prop=coordinates%7Cdescription%7Cimageinfo&generator=search&gsrnamespace=0&gsrsort=relevance&gsrsearch=" + encodeURIComponent(query));
+  var json = JSON.parse(result.body);
+  var resultArray = [];
+  if(json !== undefined && json.query !== undefined && json.query.pages !== undefined) {
+      var pages = json.query.pages;
+      for (var id in pages) {
+        if(pages[id].coordinates !== undefined) {
+            var page = {};
+            page.pageid = pages[id].pageid;
+            page.title = pages[id].title;
+            page.description = pages[id].description;
+            page.lat = pages[id].coordinates[0].lat;
+            page.lon = pages[id].coordinates[0].lon;
+            page.location = page.lat + "," + page.lon;
+            resultArray.push(page);
+        }
+      }
+  }
+  return resultArray;
+}
