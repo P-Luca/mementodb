ce9ed7ff369824fbc66d462416c48d0c8b01a51b
luca <luca.pigliapoco@emmesistemi.it>
2018-07-24 13:54:54 +0200

    Script per ricerca con google apis

---
+++ google.js
@@ -0,0 +1,30 @@
+var BASE_URL = "https://maps.googleapis.com/maps/api/place";
+
+function Google(key) {
+	this.key = key;
+}
+
+Google.prototype.getUrl = function(relativeUrl) {
+  return BASE_URL + relativeUrl + "?key=" + this.key;
+}
+
+Google.prototype.search(query) {
+	var result = http().get(this.getUrl("/findplacefromtext/json")+"&inputtype=textquery&fields=place_id,photos,icon,name,rating,geometry/location,formatted_address&language=it&input=" + encodeURIComponent(query));
+	var json = JSON.parse(result.body);
+    var resultArray = [];
+	if(json !== undefined && json.query !== undefined && json.query.pages !== undefined) {
+        var candidates = json.candidates;
+        for (var id in candidates) {
+			var candidate = {};
+			candidate.placeid = candidates[id].place_id;
+			candidate.title = candidates[id].name;
+			candidate.description = candidates[id].formatted_address;
+			candidate.location = candidates[id].geometry.location.lat+","+candidates[id].geometry.location.lon;
+			if(candidates[id].photos !== undefined && candidates[id].photos.size() > 0) {
+				candidate.photo = this.getUrl("/photo")+"&maxwidth=1280&photoreference=" + encodeURIComponent(candidates[id].photos[0].photo_reference);
+			}
+			resultArray.push(candidate);
+        }
+    }
+    return resultArray;
+}
